c03a2dbb91398f204a64ce438e46d461b51567fa
maerd-zinbieL <db1345569965@gmail.com>
2022-05-16 16:42:58 +0800

    modify home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  Home
+  <div class="warning-msg">
+    <p>
+      本网站内容仅供学习使用，如有侵权，请联系
+      <span>alyssaphacker@qq.com</span>
+    </p>
+  </div>
 </template>
-<script>
-</script>
-<style>
+<style scoped>
+.warning-msg {
+  margin: 10px 5px;
+  padding: 10px;
+  border-radius: 3px 3px 3px 3px;
+  color: #9f6000;
+  background-color: #feefb3;
+  font-size: 23px;
+}
 </style>
